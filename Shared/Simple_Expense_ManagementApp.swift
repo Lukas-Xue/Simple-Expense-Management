46180f98cd303676e343fbfeee146220f2ee8453
Lukas Xue <lukas.rxue@gmail.com>
2022-08-11 23:16:01 -0400

    Initial Commit

---
+++ Shared/Simple_Expense_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Expense_ManagementApp.swift
+//  Shared
+//
+//  Created by Lukas Xue on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Expense_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
